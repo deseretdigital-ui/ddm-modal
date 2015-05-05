7ae240234909b2b01179befcd878e11979b92f14
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-05-05 14:12:32 -0600

    Use Autoprefixer on Styles
    Use gulp-autoprefixer in our gulp task for sass.

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
 var config = require('../config');
 
 gulp.task('dist', [
@@ -15,5 +16,6 @@ gulp.task('distJs', function () {
 gulp.task('distScss', function () {
   return gulp.src(config.tasks.distScss.src)
     .pipe(sass())
+    .pipe(prefix('last 2 versions', '> 1%', 'ie 8'))
     .pipe(gulp.dest(config.tasks.distScss.dest));
 });
