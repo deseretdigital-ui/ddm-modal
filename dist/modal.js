b00ce4f2810d9ba5dbc83924ee0794accc865d0e
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-05-06 00:54:42 -0600

    Makes Work in IE8 Blegh
    Adds console polyfill and downgrades to jquery 1

--- dist/modal.js
+++ dist/modal.js
@@ -1,9 +1,30 @@
+// Console-polyfill. MIT license.
+// https://github.com/paulmillr/console-polyfill
+// Make it safe to do console.log() always.
+(function(global) {
+  'use strict';
+  global.console = global.console || {};
+  var con = global.console;
+  var prop, method;
+  var empty = {};
+  var dummy = function() {};
+  var properties = 'memory'.split(',');
+  var methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' +
+     'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' +
+     'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn').split(',');
+  while (prop = properties.pop()) if (!con[prop]) con[prop] = empty;
+  while (method = methods.pop()) if (!con[method]) con[method] = dummy;
+})(typeof window === 'undefined' ? this : window);
+// Using `this` for web workers while maintaining compatibility with browser
+// targeted script loaders such as Browserify or Webpack where the only way to
+// get to the global object is via `window`.
+
 var ksl = ksl || {};
 ksl.assets = ksl.assets || {};
 ksl.assets.modal = function () {
   console.warn('DEPRECATED: ksl.assets.modal() is deprecated. Please use ddm.modal().');
   console.groupCollapsed('...');
-  console.trace('Trace:');
+    console.trace('Trace:');
   console.groupEnd();
   return ddm.modal.apply(window, arguments);
 };
